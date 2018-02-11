b4352b793299079ccf2c225a3ce039fda02dd769
root <root@host.localdomain>
2018-02-11 11:24:03 +0800

    Add mem

---
+++ mem.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [$1 == '']; then
+	echo ;
+	ps -e -o 'comm,rsz,vsz' |  sort -nrk5 | awk '{$1} {sum1 += $2} {sum2 += $3};END {print "Total: ", sum1/1024"Mb", sum2/1024"kb"}'
+	echo "================================================================================"
+	ps -e -o 'pid,comm,args,pcpu,rsz,vsz,stime,user,uid' |  sort -nrk5
+else
+	echo ;
+	ps -e -o 'comm,rsz,vsz' | grep $1 |  sort -nrk5 | awk '{$1} {sum1 += $2} {sum2 += $3};END {print "Total: ", sum1/1024"Mb", sum2/1024"kb"}'
+	echo "================================================================================"
+	ps -e -o 'pid,comm,args,pcpu,rsz,vsz,stime,user,uid' | grep $1 |  sort -nrk5
+fi
